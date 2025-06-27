ccb91ce072742a6370cbe0e8e261f58cbc3df4ae
seino <dhtp0112@gmail.com>
2025-06-27 17:22:22 +0900

    update previous file to refactored one

--- types/06_dp/boj_9251_LCS/src/Main.java
+++ types/06_dp/boj_9251_LCS/src/Main.java
@@ -1,25 +1,27 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String A = br.readLine();
-        String B = br.readLine();
-        int aLen = A.length();
-        int bLen = B.length();
-
-        int[][] dp = new int[aLen + 1][bLen + 1];
-
-        for (int i = 0; i < aLen; i++) {
-            char a = A.charAt(i);
-            char b = B.charAt(i);
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String A = br.readLine();
+            String B = br.readLine();
+            System.out.println(lcsLength(A, B));
         }
+    }
 
-        for (int i = 1; i <= aLen; i++) {
-            char a = A.charAt(i - 1);
+    // 두 문자열의 LCS 길이 반환
+    private static int lcsLength(String A, String B) {
+        int lenA = A.length();
+        int lenB = B.length();
 
-            for (int j = 1; j <= bLen; j++) {
+        // dp[i][j] : A의 앞 i글자, B의 앞 j글자의 LCS 길이
+        int[][] dp = new int[lenA + 1][lenB + 1];
+
+        for (int i = 1; i <= lenA; i++) {
+            for (int j = 1; j <= lenB; j++) {
+                char a = A.charAt(i - 1);
                 char b = B.charAt(j - 1);
 
                 if (a == b) {
@@ -30,6 +32,6 @@ public class Main {
             }
         }
 
-        System.out.println(dp[aLen][bLen]);
+       return dp[lenA][lenB];
     }
 }
