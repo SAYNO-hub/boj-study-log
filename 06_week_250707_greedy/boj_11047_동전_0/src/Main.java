6fd2b9b8ccdbc2d0535e96c53483578dd40c7c4b
seino <dhtp0112@gmail.com>
2025-07-08 22:29:31 +0900

    refactor: 함수 분리 및 파라미터 주석 추가, 변수명 명확하게 변경

--- 06_week_250707_greedy/boj_11047_동전_0/src/Main.java
+++ 06_week_250707_greedy/boj_11047_동전_0/src/Main.java
@@ -1,30 +1,42 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+    /**
+     * Greedy : 가장 큰 동전부터 차감하여 최소 동전 개수를 계산한다.
+     * @param coins 오름차순 정렬된 동전 단위
+     * @param remains 목표 금액
+     * @return 동전 최소 개수
+     */
+    private static int minCoinCount(int[] coins, int amount) {
+        int cnt = 0;
 
-        int[] coins = new int[N];
-        for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
+        for (int i = coins.length - 1; i >= 0 ; i--) {
+            if (coins[i] > amount) continue;
+            cnt += amount / coins[i];
+            amount %= coins[i];
+
+            if (amount == 0) break;
         }
+        return cnt;
+    }
 
-        int remains = K;
-        int count = 0;
-        for (int i = N-1; i >= 0; i--) {
-            if(remains >= coins[i]) {
-                count += remains / coins[i];
-                remains %= coins[i];
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            int[] coins = new int[N];
+            for (int i = 0; i < N; i++) {
+                coins[i] = Integer.parseInt(br.readLine());
             }
-            else continue;
-        }        
 
-        System.out.println(count);
+            System.out.println(minCoinCount(coins, K));
+        }        
     }
 }
