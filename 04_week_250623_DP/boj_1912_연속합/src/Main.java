4235191685d63ea7ba0669669d8c3624467a7b9c
seino <dhtp0112@gmail.com>
2025-07-08 22:17:45 +0900

    docs: 주석 수정

--- 04_week_250623_DP/boj_1912_연속합/src/Main.java
+++ 04_week_250623_DP/boj_1912_연속합/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 int val = Integer.parseInt(st.nextToken());
 
-                // Kadane's step: 이번 원소로 시작 vs. 이어붙이기
+                // Kadane's step: 이번 원소로 시작 vs. 이어붙이기 (칸데인)
                 curSum = Math.max(val, curSum + val);
                 maxSum = Math.max(maxSum, curSum);            
             }
@@ -26,23 +26,23 @@ public class Main {
     }
 }
 
-// public static void main(String[] args) throws Exception {
-//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-//     int n = Integer.parseInt(br.readLine());
-//     int[] integers = new int[n];
-//     StringTokenizer st = new StringTokenizer(br.readLine());
-//     for (int i = 0; i < n; i++) {
-//         integers[i] = Integer.parseInt(st.nextToken());
-//     }
+    int n = Integer.parseInt(br.readLine());
+    int[] integers = new int[n];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+        integers[i] = Integer.parseInt(st.nextToken());
+    }
     
-//     int[] dp = new int[n];
-//     dp[0] = integers[0];
-//     int max = dp[0];
-//     for (int i = 1; i < n; i++) {
-//         dp[i] = Math.max(integers[i], dp[i-1] + integers[i]);
-//         max = Math.max(max, dp[i]);
-//     }
+    int[] dp = new int[n];
+    dp[0] = integers[0];
+    int max = dp[0];
+    for (int i = 1; i < n; i++) {
+        dp[i] = Math.max(integers[i], dp[i-1] + integers[i]);
+        max = Math.max(max, dp[i]);
+    }
     
-//     System.out.println(max);
-// }
+    System.out.println(max);
+}
