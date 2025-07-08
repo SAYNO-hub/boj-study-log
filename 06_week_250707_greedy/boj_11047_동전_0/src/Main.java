4798df031384bb5be8b1d3b4a32dc9117062ac1e
seino <dhtp0112@gmail.com>
2025-07-08 22:54:21 +0900

    chore: recover deleted files

---
+++ 06_week_250707_greedy/boj_11047_동전_0/src/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    /**
+     * Greedy : 가장 큰 동전부터 차감하여 최소 동전 개수를 계산한다.
+     * @param coins 오름차순 정렬된 동전 단위
+     * @param remains 목표 금액
+     * @return 동전 최소 개수
+     */
+    private static int minCoinCount(int[] coins, int amount) {
+        int cnt = 0;
+
+        for (int i = coins.length - 1; i >= 0 ; i--) {
+            if (coins[i] > amount) continue;
+            cnt += amount / coins[i];
+            amount %= coins[i];
+
+            if (amount == 0) break;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            int[] coins = new int[N];
+            for (int i = 0; i < N; i++) {
+                coins[i] = Integer.parseInt(br.readLine());
+            }
+
+            System.out.println(minCoinCount(coins, K));
+        }        
+    }
+}
