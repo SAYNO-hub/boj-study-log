54c8b614c88d01cb8ecad8ba0ae8a11225d249aa
seino <dhtp0112@gmail.com>
2025-07-19 03:43:32 +0900

    solve: BOJ 14425 문자열 집합 문제 풀이

--- 07_week_260714_hash/boj_14425_문자열 집합/src/Main.java
+++ 07_week_260714_hash/boj_14425_문자열 집합/src/Main.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Integer> words = new HashMap<>();    
+
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            words.put(word, 1);
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < M; i++) {
+            String question = br.readLine();
+
+            if (words.containsKey(question)) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
