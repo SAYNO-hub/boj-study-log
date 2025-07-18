e0e79a0420ce1ca64e5755d75286b6da43c3cd82
seino <dhtp0112@gmail.com>
2025-07-19 04:16:41 +0900

    solve: BOJ 10815 숫자 카드 문제 풀이

--- 07_week_260714_hash/boj_10815_숫자 카드/src/Main.java
+++ 07_week_260714_hash/boj_10815_숫자 카드/src/Main.java
@@ -1,5 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        HashSet <Integer> numSet = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            numSet.add(num);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (numSet.contains(num)) {
+                sb.append("1").append(" ");
+            } else {
+                sb.append("0").append(" ");
+            }
+        }
+
+        System.out.println(sb);
     }
 }
