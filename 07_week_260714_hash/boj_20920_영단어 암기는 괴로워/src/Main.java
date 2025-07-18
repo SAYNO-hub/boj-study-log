753db32be5dd4e132389b3c9dfded6983d27f10d
seino <dhtp0112@gmail.com>
2025-07-19 05:14:02 +0900

    refactor: 중복 제거 및 BufferedWriter로 출력 최적화
    - getOrDefault 사용으로 put 로직 간결화 (조건문 제거)
    - System.out 대신 BufferedWriter로 출력 속도 개선 (StringBuilder는 시간 초과는 아니지만 상대적으로 느림)
    - 타입 생략 → 구현체(HashMap 등) 대신 상위 타입 인터페이스 (Map, List 등) 사용으로 추상적이고 유연한 코드로 개선
    - 정렬 조건 주석 정리와 코드 가독성 향상

--- 07_week_260714_hash/boj_20920_영단어 암기는 괴로워/src/Main.java
+++ 07_week_260714_hash/boj_20920_영단어 암기는 괴로워/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,26 +10,25 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 추가
+
 
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
 
-        HashMap <String, Integer> wordMap = new HashMap<>();
+        Map <String, Integer> wordMap = new HashMap<>();
+        
         for (int i = 0; i < N; i++) {
             String word = br.readLine();
             if (word.length() < M)  continue;
 
-            if (!wordMap.containsKey(word)) {
-                wordMap.put(word, 1);
-            } else {
-                wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
-            }
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
         }
 
         List<Map.Entry<String, Integer>> entries = new ArrayList<>(wordMap.entrySet());
 
-        // 정렬: 빈도 내림차순 → 길이 내림차순 → 사전순 오름차순
+        // 정렬: 빈도 ↓, 길이 ↓, 사전순 ↑
         entries.sort((e1, e2) -> {
             int freqCompare = Integer.compare(e2.getValue(), e1.getValue()); // 빈도 내림차순
             if (freqCompare != 0) return freqCompare;
@@ -43,11 +44,20 @@ public class Main {
         //     System.out.println(entry.getKey());
         // }
 
-        StringBuilder sb = new StringBuilder();
+        // StringBuilder sb = new StringBuilder();
+        // for (Map.Entry<String, Integer> entry : entries) {
+        //     sb.append(entry.getKey()).append('\n');
+        // }
+
+        // System.out.print(sb);
+
+        // 출력 - 문제에서 추천하는 출력 방식
         for (Map.Entry<String, Integer> entry : entries) {
-            sb.append(entry.getKey()).append('\n');
+            bw.write(entry.getKey());
+            bw.newLine();
         }
 
-        System.out.print(sb);
+        bw.flush(); // 반드시 flush!
+        bw.close();
     }
 }
