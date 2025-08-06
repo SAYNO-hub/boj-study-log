df8e3298196be2f1fc4c171da50765ba5a7942be
seino <dhtp0112@gmail.com>
2025-08-07 05:47:15 +0900

    chore: 이분탐색을 binary search로 폴더명 변경

--- 08_week_250721_sliding-window-two-pointers/boj_2559_수열/src/Main.java
+++ 08_week_250721_sliding-window-two-pointers/boj_2559_수열/src/Main.java
@@ -1,5 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] temperature = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            temperature[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 초기 K개의 합
+        int currentSum = 0;
+        for(int i = 0; i < K; i++){
+            currentSum += temperature[i];
+        }
+        int maxSum = currentSum;
+
+        // 슬라이딩 윈도우
+        for(int i = K; i < N; i++){
+            currentSum += temperature[i] - temperature[i - K];
+            if(currentSum > maxSum) maxSum = currentSum;
+        }
+
+        System.out.println(maxSum);
     }
 }
