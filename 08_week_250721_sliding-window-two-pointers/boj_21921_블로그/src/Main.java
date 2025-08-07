28ff924bdba955a8c060f88a29300709a4db794f
seino <dhtp0112@gmail.com>
2025-08-07 18:11:32 +0900

    Revert "chore: 이분탐색을 binary search로 폴더명 변경"
    This reverts commit df8e3298196be2f1fc4c171da50765ba5a7942be.

--- 08_week_250721_sliding-window-two-pointers/boj_21921_블로그/src/Main.java
+++ 08_week_250721_sliding-window-two-pointers/boj_21921_블로그/src/Main.java
@@ -1,49 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
-
-        int[] visits = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            visits[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int currentSum = 0; // long 고민
-
-        // 초기 윈도우 합 (처음 X일)
-        for (int i = 0; i < X; i++) {
-            currentSum += visits[i];
-        }
-
-        int maxSum = currentSum; // long 고민
-        int count = 1;
-
-        // 인덱스 i는 "새로 추가되는 날"의 위치
-        for (int i = X; i < N; i++) {
-            currentSum += visits[i];
-            currentSum -= visits[i - X];
-
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-                count = 1;
-            } else if (currentSum == maxSum) {
-                count++;
-            }
-        }
-
-        if (maxSum == 0) {
-            System.out.println("SAD");
-        } else {
-            System.out.println(maxSum);
-            System.out.println(count);
-        }
+        System.out.println("Hello, World!");
     }
 }
--- 08_week_250721_sliding-window-two-pointers/boj_3273_두 수의 합/src/Main.java
+++ 08_week_250721_sliding-window-two-pointers/boj_3273_두 수의 합/src/Main.java
@@ -1,37 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        // 한 줄에 입력된 정수들을 공백 기준으로 나눠서, int 배열로 바꾸는 코드
-        int[] arr = Arrays.stream(br.readLine().split(" ")) // 공백 기준 나눔
-                          .mapToInt(Integer::parseInt) // String -> int
-                          .toArray(); // int[] 배열로 변환
-
-        int x = Integer.parseInt(br.readLine());
-        Arrays.sort(arr);
-
-        int left = 0, right = n - 1, count = 0;
-
-        while (left < right) {
-            int sum = arr[left] + arr[right];
-
-            if (sum == x) {
-                count++;
-                left++;
-                right--;
-            } else if (sum < x) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        System.out.println(count);
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
