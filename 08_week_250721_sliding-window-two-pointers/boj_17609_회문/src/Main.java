aed2beed89f4aa84c3beb746ed1f40ef0bfc8ee1
seino <dhtp0112@gmail.com>
2025-08-07 18:11:32 +0900

    Revert "chore: 이분탐색을 binary search로 폴더명 변경"
    This reverts commit df8e3298196be2f1fc4c171da50765ba5a7942be.

--- 08_week_250721_sliding-window-two-pointers/boj_17609_회문/src/Main.java
+++ 08_week_250721_sliding-window-two-pointers/boj_17609_회문/src/Main.java
@@ -1,45 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
-// 회문 : 앞에서 읽으나 뒤에서 읽으나 같은 문자열
-
 public class Main {
-    static boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right))
-                return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
-
-    static int checkPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                // 한쪽 문자를 제거하고 검사
-                if (isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)) {
-                    return 1; // 유사 회문 : 한 문자를 삭제하여 회문이 될 때
-                } else {
-                    return 2; // 회문 아님
-                }
-            }
-            left++;
-            right--;
-        }
-        return 0; // 회문
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        for (int t = 0; t < T; t++) {
-            String s = br.readLine();
-            System.out.println(checkPalindrome(s));
-        }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
--- 08_week_250721_sliding-window-two-pointers/boj_2467_용액/src/Main.java
+++ 08_week_250721_sliding-window-two-pointers/boj_2467_용액/src/Main.java
@@ -1,43 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int[] liquids = Arrays.stream(br.readLine().split(" "))
-                              .mapToInt(Integer::parseInt)
-                              .toArray();
-        // 이미 정렬된 상태로 들어와서 정렬 불필요
-        
-        int left = 0, right = N - 1;
-
-        // 두 용액의 합 절댓값 중에서 '최솟값' 찾기
-        // 최소값을 찾는 알고리즘에서 가장 큰 값으로 초기화하는 건 아주 기본적인 테크닉
-        int minAbsSum = Integer.MAX_VALUE; 
-
-        int leftValue = 0, rightValue = 0;
-
-        while (left < right) {
-            int sum = liquids[left] + liquids[right];
-            int absSum = Math.abs(sum);
-
-            if (absSum < minAbsSum) {
-                minAbsSum = absSum;
-                leftValue = liquids[left];
-                rightValue = liquids[right];
-            }
-
-            if (sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        System.out.println(leftValue + " " + rightValue);
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
