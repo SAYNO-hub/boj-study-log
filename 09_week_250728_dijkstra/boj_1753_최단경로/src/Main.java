5dab1c97cbf2c508752cfdc032bf1c0542b365f4
seino <dhtp0112@gmail.com>
2025-08-09 02:54:16 +0900

    solve: BOJ 1753 최단경로 문제풀이

--- 09_week_250728_dijkstra/boj_1753_최단경로/src/Main.java
+++ 09_week_250728_dijkstra/boj_1753_최단경로/src/Main.java
@@ -1,5 +1,83 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
+
+    static final int INF = Integer.MAX_VALUE;
+    static ArrayList<Node>[] graph;
+    static int[] distance;
+
+    static class Node implements Comparable<Node> {
+        int index; // 목적지 노드 번호
+        int dist; // 가중치 (거리)
+
+        Node(int index, int dist) {
+            this.index = index;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.dist, other.dist);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken()); // 정점 개수 V
+        int E = Integer.parseInt(st.nextToken()); // 간선 개수 E
+        int K = Integer.parseInt(br.readLine()); // 시작 정점의 번호 K
+
+        distance = new int[V + 1];
+        Arrays.fill(distance, INF);
+
+        graph = new ArrayList[V + 1];
+        for (int i = 0; i <= V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            graph[u].add(new Node(v, w));
+        }
+
+        dijkstra(K);
+
+        for (int i = 1; i <= V; i++) {
+            if (distance[i] == INF)
+                System.out.println("INF");
+            else
+                System.out.println(distance[i]);
+        }
+    }
+
+    static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        distance[start] = 0;
+        pq.offer(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            int now = current.index;
+
+            if (current.dist > distance[now])
+                continue; // 이미 처리된 경우 건너뛰기
+
+            for (Node next : graph[now]) {
+                int cost = distance[now] + next.dist;
+
+                if (cost < distance[next.index]) {
+                    distance[next.index] = cost;
+                    pq.offer(new Node(next.index, cost));
+                }
+            }
+        }
     }
 }
